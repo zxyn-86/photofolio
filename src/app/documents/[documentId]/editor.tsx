e38ab2d45f4880768a7a8641ddb7468679d59b8e
zxu598 <zxu598@student.bham.ac.uk>
2026-03-01 15:08:54 +0000

    styles added for editor funcs

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -1,7 +1,14 @@
 'use client';
 
 import { useEditor, EditorContent } from '@tiptap/react'
+import Table from '@tiptap/extension-table'
+import TableCell from '@tiptap/extension-table-cell'
+import TableHeader from '@tiptap/extension-table-header'
+import TableRow from '@tiptap/extension-table-row'
+import TaskItem from '@tiptap/extension-task-item'
+import TaskList from '@tiptap/extension-task-list'
 import StarterKit from '@tiptap/starter-kit'
+
 
 
 export const Editor = () => {
@@ -11,13 +18,39 @@ export const Editor = () => {
 
             attributes:{
                 style: "padding-left: 56px; padding-right: 56px;",
-                class: "focus:outline-none print:border-0 bg-white border border-[#C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text"
+                class: "focus:outline-none print:border-0 bg-white border border-[#C7C7c7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text"
 
             },
 
     },
-    extensions: [StarterKit],
-    content: '<p>Hello World! 🌎️</p>',
+    extensions: [
+        StarterKit,
+        TaskItem.configure({
+            nested:true,
+        }),
+        TaskList,
+        Table,
+        TableCell,
+        TableHeader,
+        TableRow,
+
+    ],
+    content: `
+        <table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th colspan="3">Description</th>
+            </tr>
+            <tr>
+              <td>Cyndi Lauper</td>
+              <td>Singer</td>
+              <td>Songwriter</td>
+              <td>Actress</td>
+            </tr>
+          </tbody>
+        </table>
+      `,
     })
     return (
         <div className = "size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
